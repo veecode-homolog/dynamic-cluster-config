2564e0e11bb6a4d4a3f5f01d7b0497aa5365a2df
Bruno <bn.brunosantos@gmail.com>
2024-08-27 21:15:39 -0300

    fix: adjust service account script

--- k8s-service-account/apply.sh
+++ k8s-service-account/apply.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# check if namespace vkpr exists and create it if not
+# Verifica se o namespace vkpr existe e cria se não existir
 kubectl get namespace vkpr || kubectl create namespace vkpr
 
-kubectl apply -f ./cluster-role.yaml
-kubectl apply -f ./service-account.yaml
-kubectl apply -f ./cluster-role-binding.yaml
+# Aplica os arquivos YAML usando os caminhos corretos
+kubectl apply -f ./k8s-service-account/cluster-role.yaml
+kubectl apply -f ./k8s-service-account/service-account.yaml
+kubectl apply -f ./k8s-service-account/cluster-role-binding.yaml
 
-SERVICE_ACCOUNT_NAME=$(cat ./service-account.yaml | yq -e '.metadata.name')
-SERVICE_ACCOUNT_NAMESPACE=$(cat ./service-account.yaml | yq -e '.metadata.namespace // "default"')
+# Extrai o nome e o namespace da Service Account dos arquivos YAML
+SERVICE_ACCOUNT_NAME=$(cat ./k8s-service-account/service-account.yaml | yq -e '.metadata.name')
+SERVICE_ACCOUNT_NAMESPACE=$(cat ./k8s-service-account/service-account.yaml | yq -e '.metadata.namespace // "default"')
+
 echo "Generating token for $SERVICE_ACCOUNT_NAME namespace $SERVICE_ACCOUNT_NAMESPACE"
 SERVICE_ACCOUNT_TOKEN=$(kubectl create token ${SERVICE_ACCOUNT_NAME} -n ${SERVICE_ACCOUNT_NAMESPACE})
 
-## Getting current server authority certificate
+# Obtém o certificado de autoridade atual do servidor
 K8S_SERVER_CERTIFICATE=$(cat ~/.kube/config | yq -r ".clusters[] | select(.name == \"$(cat ~/.kube/config | yq -r '.current-context')\").cluster.certificate-authority-data")
 K8S_SERVER_HOST=$(cat ~/.kube/config | yq -r ".clusters[] | select(.name == \"$(cat ~/.kube/config | yq -r '.current-context')\").cluster.server")
+
 echo "-----------------------------------"
 echo "K8S Server Host: $K8S_SERVER_HOST"
 echo "-----------------------------------"
